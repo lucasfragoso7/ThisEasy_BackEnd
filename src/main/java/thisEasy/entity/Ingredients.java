d0e70d251284ea88933f4a9cd79bce08175f99f4
andre fragoso <andre.moreira@ctis.com.br>
2019-11-25 02:57:35 -0300

    Criação dos padrões de autenticação

--- src/main/java/thisEasy/entity/Ingredients.java
+++ src/main/java/thisEasy/entity/Ingredients.java
@@ -1,13 +1,9 @@
 package thisEasy.entity;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.ManyToMany;
 import javax.persistence.Table;
 import javax.validation.constraints.NotNull;
 
@@ -26,11 +22,6 @@ public class Ingredients implements EntityContract {
 	@NotNull
 	private String name;
 
-	@ManyToMany(mappedBy = "ingredients")
-	private List<Recipe> recipe = new ArrayList<>();
-
-	@ManyToMany(mappedBy = "ingredients")
-	private List<Ambry> ambry = new ArrayList<>();
 
 
 
@@ -49,23 +40,6 @@ public class Ingredients implements EntityContract {
 	public void setName(String name) {
 		this.name = name;
 	}
-
-	public List<Recipe> getRecipe() {
-		return recipe;
-	}
-
-	public void setRecipe(List<Recipe> recipe) {
-		this.recipe = recipe;
-	}
-
-	public List<Ambry> getAmbry() {
-		return ambry;
-	}
-
-	public void setAmbry(List<Ambry> ambry) {
-		this.ambry = ambry;
-	}
-	
 
 
 }
